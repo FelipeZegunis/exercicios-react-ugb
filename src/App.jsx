59bd42e71cda537a9ef42e2a6d30843593d4859f
unknown <felipegabrielvr548@gmail.com>
2026-09-16 16:58:24 -0300

    feat: sistema de caixa de mercado

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import CaixaDeMercado from './components/CaixaDeMercado'
 import JogoAdivinhacao from './components/JogoAdivinhacao'
 import PedraPapelTesoura from './components/PedraPapelTesoura'
 
-// IMPORTANTE:
-// Em cada branch (feat-exercicio1, feat-exercicio2, feat-exercicio3),
-// deixe renderizado APENAS o componente daquele exercício.
-// Troque a linha abaixo conforme a branch atual.
 
 function App() {
   return (
