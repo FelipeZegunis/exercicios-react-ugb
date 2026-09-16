a38c829981fed56d3c9034e32da9209ae7500612
unknown <felipegabrielvr548@gmail.com>
2026-09-16 17:00:42 -0300

    feat: pedra, papel e tesoura

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import CaixaDeMercado from './components/CaixaDeMercado'
 import JogoAdivinhacao from './components/JogoAdivinhacao'
 import PedraPapelTesoura from './components/PedraPapelTesoura'
 
-// IMPORTANTE:
-// Em cada branch (feat-exercicio1, feat-exercicio2, feat-exercicio3),
-// deixe renderizado APENAS o componente daquele exercício.
-// Troque a linha abaixo conforme a branch atual.
-
 function App() {
   return (
     <div className="app">
-      <CaixaDeMercado />
+      {/* <CaixaDeMercado /> */}
       {/* <JogoAdivinhacao /> */}
-      {/* <PedraPapelTesoura /> */}
+      <PedraPapelTesoura />
     </div>
   )
 }
