e685571474d2f3e74a3eb21215115d339a4f1bcd
unknown <felipegabrielvr548@gmail.com>
2026-09-16 17:12:22 -0300

    chore: limpar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import CaixaDeMercado from './components/CaixaDeMercado'
-import JogoAdivinhacao from './components/JogoAdivinhacao'
 import PedraPapelTesoura from './components/PedraPapelTesoura'
 
 function App() {
   return (
     <div className="app">
-      {/* <CaixaDeMercado /> */}
-      {/* <JogoAdivinhacao /> */}
       <PedraPapelTesoura />
     </div>
   )
